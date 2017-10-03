051317e50979973465102d57b86e9299686268fe
Daniel Bolivar herrera <dbolivarh@gmail.com>
2017-10-03 21:42:44 +0200

    Project creation

---
+++ eDairy/View Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eDairy
+//
+//  Created by Daniel Bolivar herrera on 3/10/17.
+//  Copyright © 2017 R3PI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
